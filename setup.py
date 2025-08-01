5da8cfa3a506ced48b87e6032d61668772a21f82
Smiley-max <smileyelliotbachsmith@gmail.com>
2025-08-01 12:35:21 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smileyfix',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A Python library that makes Python easier',
+    author='Elliot',
+    author_email='',
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
